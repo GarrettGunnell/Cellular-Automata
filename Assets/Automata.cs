e092bb8cab1ca2975344b61646966ba4c8f54d21
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-09-19 03:31:29 -0700

    Verify compute shader works

--- Assets/Automata.cs
+++ Assets/Automata.cs
@@ -7,11 +7,12 @@ public class Automata : MonoBehaviour {
     public ComputeShader automataCompute;
 
     private RenderTexture target;
-    private int kernel, threadGroupsX, threadGroupsY;
+    private int kernel, threadGroupsX, threadGroupsY, generation;
     
     void Awake() {
         threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        generation = Screen.height - 2;
     }
 
     void OnEnable() {
@@ -22,10 +23,16 @@ public class Automata : MonoBehaviour {
         }
     }
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+    void Update() {
         automataCompute.SetTexture(0, "_Result", target);
+        automataCompute.SetInt("_Generation", generation);
         automataCompute.Dispatch(0, threadGroupsX, threadGroupsY, 1);
 
+        if (generation > 0)
+            generation--;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
         Graphics.Blit(target, destination);
     }
 }
