5e5acfb269e4c7fb488ab8b29b55cf26798c97e7
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-09-19 03:04:31 -0700

    Dispatch default compute shader

--- Assets/Automata.cs
+++ Assets/Automata.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Automata : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Automata : MonoBehaviour {
+    
+    public ComputeShader automataCompute;
+
+    private RenderTexture target;
+    private int kernel, threadGroupsX, threadGroupsY;
+    
+    void Awake() {
+        threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnEnable() {
+        if (target == null) {
+            target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        automataCompute.SetTexture(0, "_Result", target);
+        automataCompute.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        Graphics.Blit(target, destination);
     }
 }
